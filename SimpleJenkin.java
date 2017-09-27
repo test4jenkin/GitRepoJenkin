ee5a03d2afd138697250f0030a1bee0a2c4e5d01
Amit Hada <amit.hada@metacube.com>
2017-09-27 21:32:08 +0530

    update injenkinsetup

--- SimpleJenkin.java
+++ SimpleJenkin.java
@@ -1,6 +1,6 @@
 class SimpleJenkin{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java-jenkinsetupbranch");  
 	 
 	 System.out.println("Commit-1:Hello Java");  
     }  
